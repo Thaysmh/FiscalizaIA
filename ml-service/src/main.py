5034f01c6c3d9ddf61edc5fa015e083a5e4529cc
thays <thays.nave@gmail.com>
2026-06-23 15:26:17 -0300

    Classificando uma quantidade mínima de contratos como 0: NAO_CREA e 1: CREA.

--- ml-service/src/main.py
+++ ml-service/src/main.py
@@ -8,5 +8,18 @@ dados_api = response.json()
 contratos = dados_api["data"]["dados"]
 df = pd.DataFrame(contratos)
 
-print(df["Objeto"].iloc[0])
-print(df[["Código", "Objeto"]].head())
+#print(df["Objeto"].iloc[0])
+#print(df[["Código", "Objeto"]].head())
+
+df = df[
+    [
+        "Código",
+        "Objeto",
+        "TCO_Descricao",
+        "Lic_Descricao",
+        "Valor"
+    ]
+]
+df["Categoria"] = ""
+
+df.to_csv("contratos_rotular.csv", index=False)
