3cc315732845adef224f8fad6f3b38bb44a689a5
thays <thays.nave@gmail.com>
2026-06-23 13:10:56 -0300

    Fazendo main.py consumir dados da api node.

---
+++ ml-service/src/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import requests
+
+response = requests.get("http://localhost:3000/api/prefeituras/duque-de-caxias?year=2026")
+
+dados_api = response.json()
+
+contratos = dados_api["data"]["dados"]
+df = pd.DataFrame(contratos)
+
+print(df["Objeto"].iloc[0])
+print(df[["Código", "Objeto"]].head())
